f160e55c48437c80c7db27206530ada34cf7ca21
Carlos Cosming <carlos@kingly.cl>
2024-10-03 00:30:53 -0300

    🔧 chore: add google remotePatterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,13 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.us-west-2.amazonaws.com',
         port: '',
         pathname: '/**',
       },
